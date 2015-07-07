efcdd4fd606831a6117d27cbe0e28dcd01b45031
Diccon Bate <diccon@dicconbate.co.uk>
2015-07-07 13:13:05 +0000

    starting section 3.2

--- Lab3W.ps1
+++ Lab3W.ps1
@@ -101,3 +101,50 @@ c:\temp\snapshotter.ps1 "eu-west-1"
 #aws ec2 describe-snapshots --filters "Name=volume-id,Values=<volume-id>" --query 'Snapshots[*].SnapshotId'
 aws ec2 describe-snapshots --filters "Name=volume-id,Values=vol-cbc9d9dc" --query 'Snapshots[*].SnapshotId'
 
+<#
+==================================================================================================================
+#3.2 Move a Log File into Amazon S3
+==================================================================================================================
+#>
+
+#3.2.1 Configure the AWS CLI
+
+aws configure
+
+#3.2.3 Download loggen.sh
+
+(New-Object System.Net.WebClient).DownloadFile("https://d2lrzjb0vjvpn5.cloudfront.net/sys-ops/v2.2/lab-3-storage-windows/static/loggen.ps1", "c:\temp\loggen.ps1")
+
+#3.2.4 Run loggen.ps1
+
+c:\temp\loggen.ps1
+
+#3.2.7 Change directories
+
+cd \temp
+
+#3.2.8 Move file into Amazon S3
+
+aws s3 mv timestamp.log "s3://<s3-bucket-name>/logfiles/timestamp-$(Get-Date -Format 'M-d-yyyy-h:m:s')"
+
+#3.2.9 Move file again
+
+aws s3 mv timestamp.log "s3://<s3-bucket-name>/logfiles/timestamp-$(Get-Date -Format 'M-d-yyyy-h:m:s')"
+
+#3.2.10 List the contents of your Amazon S3 bucket
+
+aws s3 ls s3://<s3-bucket-name>
+
+#3.2.11 List all objects containing the /logfiles/ prefix
+
+aws s3 ls s3://<s3-bucket-name>/logfiles/
+
+#3.2.12 Move oldest file to /logfiles/archive/ prefix
+
+aws s3 mv s3://<s3-bucket-name>/logfiles/<file-name> s3://<s3-bucket-name>/logfiles/archive/<file-name>
+
+#3.2.13 Verify move
+
+aws s3 ls s3://<s3-bucket-name>/logfiles/
+
+
